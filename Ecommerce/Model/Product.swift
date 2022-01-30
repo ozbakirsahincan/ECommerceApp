136257c512f7932f64beb43c8aa43480e2f36d5b
sahincan <ozbakirsahincan@gmail.com>
2022-01-30 12:13:19 +0300

    Product Ekranları eklendi

---
+++ Ecommerce/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  Ecommerce
+//
+//  Created by Şahincan Özbakır on 30.01.2022.
+//
+
+import SwiftUI
+
+struct Product: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Product_Previews: PreviewProvider {
+    static var previews: some View {
+        Product()
+    }
+}
---
+++ Ecommerce/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  Ecommerce
+//
+//  Created by Şahincan Özbakır on 30.01.2022.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
